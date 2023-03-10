ca64830a2139aaaeb41ab29cee9b4f4125f9245a
softmax0112 <softmax0112@gmail.com>
2023-03-10 10:09:14 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # golang-doc-projects
 Golang projects for Learning in Tutorials Documentation
+
+## List of projects
+1. hello-project
+2. workspace
+3. web-service-gin
+4. generics
---
+++ hello-project/greetings/greetings.go
@@ -0,0 +1,39 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// init sets initial values for variables used in the functions.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	// A slice of message formats
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
+}
